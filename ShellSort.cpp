aa8250de407d01cd67b2458309c7411645e2d403
saman <sammyp@smu.edu>
2022-09-22 20:34:11 -0500

    created project

--- ShellSort.cpp
+++ ShellSort.cpp
@@ -1,25 +1,56 @@
 #include <vector>
+#include <cmath>
 #include "c1.h"
 
 void ShellSort(vector<C1>& varray, int code)
 {
+    vector<int> hlist;
+    int n = varray.size();
 
-    if(code == 0){
-        //do nothing, array is sorted
+    switch(code){
+        case 0: //code = 0
+            hlist.push_back(1);
+            break;
+
+        case 1: { //code = 1
+            int k = floor(log2(n));
+            for(int i = k; i >= 0; i--){
+                double j = pow(2, i);
+                hlist.push_back(j);
+            }
+        }
+            break;
+
+        case 2: { //code = 2
+            int k = floor(log2(n));
+            for(int i = k; i >= 0; i--){
+                double j = pow(2, i);
+                double p = j - 1;
+                hlist.push_back(p);
+            }
+            hlist.pop_back();
+        }
+            break;
+
+        case 3: { //code = 3
+            //k (k-1) < n
+        }
+            break;
     }
 
-    if(code == 1){
-
+    for(int i = 0; i < hlist.size(); i ++) {
+        int h = hlist[i];
+        for (int j = 1; j < hlist.size(); j++) {
+            C1 key;
+            key == varray[j];
+            int i = j - h;
+            while ((i > 0) and (key < varray[i])){
+                varray[i + h] = varray[i];
+                i = i - h;
+            }
+        varray[i + h] == key;
+        }
     }
-
-    if(code == 2){
-
-    }
-
-    if(code == 3){
-
-    }
-
-    cout << "Shell sort \n";
+    cout << "Shell Sort"<< endl;
 }
 
