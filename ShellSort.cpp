400ec3fac608d002b6d8bea8545cf83119ec0e05
saman <sammyp@smu.edu>
2022-09-23 20:18:25 -0500

    created project

--- ShellSort.cpp
+++ ShellSort.cpp
@@ -3,64 +3,66 @@
 #include <algorithm>
 #include "c1.h"
 
-void ShellSort(vector<C1>& varray, int code)
+void ShellSort(vector<C1> varray, int code)
 {
     vector<int> hlist;
     int n = varray.size();
 
     switch(code){
-        case 0: //code = 0
+        case 0: {//code = 0
             hlist.push_back(1);
             break;
+        }
 
         case 1: { //code = 1
             int k = floor(log2(n));
-            for(int i = k; i >= 0; i--){
+            for (int i = k; i >= 0; i--) {
                 double j = pow(2, i);
                 hlist.push_back(j);
             }
-        }
             break;
+        }
 
         case 2: { //code = 2
             int k = floor(log2(n));
-            for(int i = k; i >= 0; i--){
+            for (int i = k; i >= 0; i--) {
                 double j = pow(2, i);
                 double p = j - 1;
                 hlist.push_back(p);
             }
             hlist.pop_back();
-        }
             break;
+        }
 
         case 3: { //code = 3
-            //k * (k-1) < n
+//            k * (k-1) < n
             int k = 2;
-            int temp = ((k * (k-1))/ 2);
-            while(temp < n){
+            int temp = ((k * (k - 1)) / 2);
+            while (temp < n) {
                 hlist.push_back(temp);
-                k ++;
-                temp = ((k * (k-1))/ 2);
+                k++;
+                temp = ((k * (k - 1)) / 2);
             }
             reverse(hlist.begin(), hlist.end());
-        }
             break;
+        }
 
     }
 
 
-    //print hlist
-    cout<< "Case: " << code << "The hlist is: ";
-    for(int i = 0; i < hlist.size(); i++){
-        cout<< hlist[i] << " " ;
-    }
-        cout<<endl;
+//    test print hlist
+//    cout << varray.size() << endl;
+//    cout<< "Case: " << code << " The hlist is: ";
+//    for(int i = 0; i < hlist.size(); i++){
+//        cout<< hlist[i] << " " ;
+//    }
+//        cout<<endl;
 
 
-    //shell sort:
-    for(int i = 0; i < hlist.size(); i ++) {
-        int h = hlist[i];
-        for (int j = h; j < hlist.size(); j+= h) {
+//    shell sort:
+    for(int k = 0; k < hlist.size(); k++) {
+        int h = hlist[k];
+        for (int j = 1; j < varray.size(); j++) {
             C1 key;
             key == varray[j];
             int i = j - h;
@@ -68,7 +70,7 @@ void ShellSort(vector<C1>& varray, int code)
                 varray[i + h] == varray[i];
                 i = i - h;
             }
-        varray[i + h] == key;
+            varray[i + h] == key;
         }
     }
 
--- main.cpp
+++ main.cpp
@@ -2,30 +2,46 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
-#include "ShellSort.cpp"
 #include <fstream>
+#include "ShellSort.cpp"
 
 
 int main()
 {
     vector<C1> varray;
 
-    std::ofstream case0("../case0.csv", std::ofstream::out);
-    std::ofstream case1("../case1.csv", std::ofstream::out);
-    std::ofstream case2("../case2.csv", std::ofstream::out);
-    std::ofstream case3("../case3.csv", std::ofstream::out);
+    std::ofstream case0("../case0.csv", ios::app);
+    std::ofstream case1("../case1.csv", ios::app);
+    std::ofstream case2("../case2.csv", ios::app);
+    std::ofstream case3("../case3.csv", ios::app);
 
 
     //srand for generating C1 objects
     srand(time(NULL));
+    int n = 100000;
+    for(int j = 500; j <= 5000; j+= 500) {
+        for (int i = 0; i <= 23; i++) {
+            for (int p = 0; p < j; p++) {
+                varray.push_back(C1(int(rand() %n), int(rand() %n), int(rand() % n)));
+            }
 
-    for(int i = 0; i <= 24; i++) {
-        varray.push_back(C1(int (rand() %500), int (rand() %500), int (rand() %500)));
+            C1::compareCount = 0;
+            ShellSort(varray, 0);
+            case0 << j << ',' << C1::compareCount << endl;
+
+            C1::compareCount = 0;
+            ShellSort(varray, 1);
+            case1 << j << ',' << C1::compareCount << endl;
+
+            C1::compareCount = 0;
+            ShellSort(varray, 2);
+            case2 << j << ',' << C1::compareCount << endl;
+
+            C1::compareCount = 0;
+            ShellSort(varray, 3);
+            case3 << j << ',' << C1::compareCount << endl;
+
+            varray.clear();
+        }
     }
-
-        ShellSort(varray, 3);
-
-
-        //clear compare count::: C1().compareCount = 0;
-
-    }
+}
